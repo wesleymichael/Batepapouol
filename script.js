10d34b2cf6f08ec18e0f9471315771f11f5e5c85
Wesley <wesleymichaelpereira@hotmail.com>
2023-01-19 01:44:41 -0300

    Layout do chat + validação do nome de usuário

--- script.js
+++ script.js
@@ -1,31 +1,70 @@
+/**** VARIÁVEIS GLOBAIS ****/
+let userName;
 
 
-function sucessRequest(response){
+
+
+function loadMessagesSucess(sucess){
+    console.log(sucess)
+    //Implementar sucesso - Renderizar mensagens do servidor
+}
+
+
+function loadMessagesError(error){
+    console.log(error)
+    //Implementar erro
+}
+
+
+function loadMessages(){
+    const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+
+    promise.then(loadMessagesSucess);
+    promise.catch(loadMessagesError);
+}
+
+
+///////////////////////////// INICIO DA IMPLEMENTAÇÃO DO CHAT (acima) ///////////////////////////////////////////////////
+
+
+/**** Função para tratar resposta com sucesso ao enviar nome do usuário para o servidor ****/
+function userSucessRequest(sucess){
     //Status = 200, usuário pode conectar. Ir para a página de mensagens
     //Dúvida: sempre que eu precisar do meu nome de usuário preciso usar o GET??
-    alert('Pode entrar')
-
+    //console.log(sucess.status)
+    window.location.href = "chat.html"
     //Redirecionar para a página de mensagens
-    console.log(response.status);
 }
 
 
-function error(response){
+/**** Função para tratar erro do envio do nome do usuário para o servidor ****/
+function errorUser(erro){
     //Status 400 = nome de usuário já em uso
     //Pedir pra escolher outro nome
-    const login = document.querySelector('.login');
-    login.innerHTML = `
-        <input type="text" placeholder="Digite seu nome">
-        <button type="submit" name="button" onclick="logIn()" disabled>Entrar</button>
-    `;    
-
-    console.log(response.status);
+    if (erro.status === 400){
+        alert('Já existe alguém com esse nome.');
+        const login = document.querySelector('.login');
+        login.innerHTML = `
+            <input type="text" placeholder="Digite seu nome">
+            <button type="submit" name="button" onclick="logIn()" disabled>Entrar</button>
+        `;
+    }
+    else{
+        alert('Ocorreu um erro inexperado');
+        const login = document.querySelector('.login');
+        login.innerHTML = `
+            <input type="text" placeholder="Digite seu nome">
+            <button type="submit" name="button" onclick="logIn()" disabled>Entrar</button>
+        `;
+    }
 }
 
 
+/**** Função para solicitar entrada na sala de bate papo ****/
 function logIn(){
+    userName = document.querySelector('input').value;
     const dados = {
-        name: document.querySelector('input').value,
+        name: userName,
     };
 
     const login = document.querySelector('.login');
@@ -34,13 +73,15 @@ function logIn(){
         <span>Entrando...</span>
     `;
 
-    const promisse = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', dados);
+    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', dados);
 
-    promisse.then(sucessRequest);
-    promisse.catch(error);
+    promise.then(userSucessRequest);
+    promise.catch(errorUser);
 }
 
 
+
+/**** Função para habilitar o botão quando for digitado pelo menos 3 caracteres ****/
 function validateInput({target}){
     const button = document.querySelector('button');
     if(target.value.length > 2){
